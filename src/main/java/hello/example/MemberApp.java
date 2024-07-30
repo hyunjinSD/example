8b40400d8f953394023fdca5651ff6b047ad1371
hyunjinSD <hyenjin09@nate.com>
2024-07-30 22:44:07 +0900

    study project

---
+++ src/main/java/hello/example/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.example;
+
+import hello.example.member.Grade;
+import hello.example.member.Member;
+import hello.example.member.MemberService;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
