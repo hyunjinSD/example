2310bb6e6fa17fbf02af2e3ca00deee8d0f05506
hyunjinSD <hyenjin09@nate.com>
2024-08-02 12:36:36 +0900

    study project

---
+++ src/main/java/hello/example/order/OrderServiceImpl.java
@@ -0,0 +1,25 @@
+package hello.example.order;
+
+import hello.example.discountpolicy.DiscountPolicy;
+import hello.example.member.Member;
+import hello.example.member.MemberRepository;
+import hello.example.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member,itemPrice);
+
+        return new Order(memberId, itemName, itemPrice,discountPrice);
+    }
+}
