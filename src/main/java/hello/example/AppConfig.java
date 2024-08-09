6365d054ce27e43049a5d1c49452c8a910df1c9a
hyunjinSD <hyenjin09@nate.com>
2024-08-09 16:52:35 +0900

    Configuration

--- src/main/java/hello/example/AppConfig.java
+++ src/main/java/hello/example/AppConfig.java
@@ -8,18 +8,25 @@ import hello.example.member.MemberServiceImpl;
 import hello.example.member.MemoryMemberRepository;
 import hello.example.order.OrderService;
 import hello.example.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
